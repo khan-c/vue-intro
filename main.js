01b8fe69fd77fc384eafc5e4e34b5ec079178b92
Kyle Chen <kyle.h.chen@gmail.com>
2020-01-24 17:37:00 -0800

    Conditional rendering

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks',
     image: './assets/vmSocks-green.jpg',
-    link: 'www.google.com',
+    inStock: true,
   }
 })
